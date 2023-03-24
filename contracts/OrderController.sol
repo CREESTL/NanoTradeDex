750739861f3622ef36a333284bd7c88c2c7cde36
degorov <degorov@sfxdx.com>
2023-03-24 16:55:08 +0300

    Add a separate sell limit function.

--- contracts/OrderController.sol
+++ contracts/OrderController.sol
@@ -253,6 +253,7 @@ contract OrderController is IOrderController, Ownable, ReentrancyGuard {
         }
 
 
+        // User has to lock exactly the amount of `tokenB` he is selling
         uint256 lockAmount = amount;
 
         // Calculate the fee amount for the order
@@ -338,8 +339,61 @@ contract OrderController is IOrderController, Ownable, ReentrancyGuard {
         );
     }
 
-    function sellLimit() external {
+    function sellLimit(
+        address tokenA,
+        address tokenB,
+        uint256 amount,
+        uint256 limitPrice,
+        bool isCancellable,
+        uint256 nonce,
+        bytes calldata signature
+    ) external {
 
+        // Form args structure to pass it to `createOrder` function later
+        OrderArgs memory args = OrderArgs(
+            tokenA,
+            tokenB,
+            amount,
+            // Initial amount is always 0
+            0,
+            OrderType.Limit,
+            OrderSide.Sell,
+            limitPrice,
+            // Slippage is 0 for limit orders
+            0,
+            // Leave 0 for lockAmount and feeAmount for now
+            0,
+            0,
+            isCancellable,
+            nonce,
+            signature
+        );
+
+        require(args.tokenA != address(0), "OC: Cannot buy native tokens!");
+        require(args.amount != 0, "OC: Cannot buy/sell zero tokens!");
+
+        // If none of the tokens is quoted, `tokenB_` becomes a quoted token
+        if (!isQuoted[args.tokenA][args.tokenB] && !isQuoted[args.tokenB][args.tokenA]) {
+            isQuoted[args.tokenA][args.tokenB] = true;
+        }
+
+        // User has to lock exactly the amount of `tokenB` he is selling
+        uint256 lockAmount = args.amount;
+
+        // Calculate the fee amount for the order
+        uint256 feeAmount = _getFee(lockAmount);
+
+        // Mark that fee amount of `tokenB` was increased
+        tokenFees[args.tokenB] += feeAmount;
+        lockedTokens.add(args.tokenB);
+
+        // Set the real fee and lock amounts
+        args.feeAmount = feeAmount;
+        args.lockAmount = lockAmount;
+
+        _createOrderMinimal(
+            args
+        );
     }
 
     // TODO replace it with buyMarket, sellMarket ...
