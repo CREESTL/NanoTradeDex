80b58a92506822645a405d63a79e187e7d20cc20
degorov <degorov@sfxdx.com>
2023-03-27 20:25:42 +0300

    Remove migrations.

--- migrations/2_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const OrderController = artifacts.require('OrderController');
-
-module.exports = function (deployer) {
-  deployer.then(async () => {
-    await deployer.deploy(OrderController, 25);
-  });
-};
